4ac837b164e030462a372b407e1fd9547bd3c050
Skyler Aschenbeck <aschenbeck.skyler@gmail.com>
2020-11-23 16:58:37 -0600

    first commit

---
+++ src/test/java/codeup/com/springstarter/SpringstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package codeup.com.springstarter;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstarterApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
